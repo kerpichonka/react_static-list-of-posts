4177f4367ee00f3416ef62d9710db8d8242ee94d
Dima <dimontregyb@email.ua>
2021-01-17 15:52:28 +0200

    Updates

---
+++ precache-manifest.4509a3f261bac9ace6607d87c6e6aa36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5565e048ca9636474007d4be7fa2f0d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39e353b7aaec76c4f8e9",
+    "url": "./static/css/main.aed50a1d.chunk.css"
+  },
+  {
+    "revision": "98dc2684b032f1ed258a",
+    "url": "./static/js/2.996e8c79.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.996e8c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e353b7aaec76c4f8e9",
+    "url": "./static/js/main.3fec8068.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  },
+  {
+    "revision": "7488ca819cbf1f7a8cb02f3778e45c99",
+    "url": "./static/media/quotes.png.7488ca81.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.4509a3f261bac9ace6607d87c6e6aa36.js"
 );
 
 self.addEventListener('message', (event) => {
